091689c321f3b42d1e7d6860f626246d00a8212c
Lucas Emmanuel <63796828+lucasemmanuelferreiradearaujo@users.noreply.github.com>
2020-06-26 12:50:05 -0300

    criação das aulas

---
+++ aulaListaP2/aula84.py
@@ -0,0 +1,6 @@
+"""
+Faça um programa que leia nome e peso de várias pessoas, guardando tudo em uma lista. No final, mostre:
+A) Quantas pessoas foram cadastradas.
+B) Um listagem com as pessoas mais pesadas.
+C) Uma lista com as pesssoas mais leves.
+"""
---
+++ aulaListaP2/aula85.py
@@ -0,0 +1,3 @@
+"""
+Crie um programa onde o usuário possa digitr sete valores numéricos e cadastre-os em uma lista únicas que mantenha separados os valores pares e impares. No final, mostre os valores pares e impares em ordem crescente.
+"""
