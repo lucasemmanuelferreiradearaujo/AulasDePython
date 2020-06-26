5a0b8143b8c47cd1aade181c5ea888d82266fc25
Lucas Emmanuel <63796828+lucasemmanuelferreiradearaujo@users.noreply.github.com>
2020-06-26 13:22:41 -0300

    Minha resolução

--- aulaListaP2/aula84.py
+++ aulaListaP2/aula84.py
@@ -4,3 +4,21 @@ A) Quantas pessoas foram cadastradas.
 B) Um listagem com as pessoas mais pesadas.
 C) Uma lista com as pesssoas mais leves.
 """
+pesoMaior = []
+pesoMenor = []
+cont = 0
+
+while True:
+    aux = int(input('Qual é o seu peso? '))
+    cont+=1
+    if aux >= 100:
+        pesoMaior.append(aux)
+    else:70    
+        pesoMenor.append(aux)
+    op = str(input('Continuar? [S/N] '))
+    if op in 'N':
+        break
+
+print(cont)
+print(pesoMaior)
+print(pesoMenor)
