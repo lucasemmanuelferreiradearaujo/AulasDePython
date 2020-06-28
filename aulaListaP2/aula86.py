85d199481ef56d17b6709a56536c24c4fb7aa9af
Lucas Emmanuel <63796828+lucasemmanuelferreiradearaujo@users.noreply.github.com>
2020-06-27 21:39:39 -0300

    sulução do professor :/

--- aulaListaP2/aula86.py
+++ aulaListaP2/aula86.py
@@ -3,3 +3,13 @@ Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores lid
 
 No final, mostre a matriz na tela, com a formatação correta.
 '''
+
+matriz = [[0,0,0],[0,0,0],[0,0,0]]
+for l in range(0,3):
+    for c in range(0,3):
+        matriz[l][c] = int(input(f'Qual o valor de [{l},{c}]: '))
+
+for l in range(0,3):
+    for c in range(0,3):
+        print(f'{matriz[l][c]:^5}', end=' ')
+    print()
