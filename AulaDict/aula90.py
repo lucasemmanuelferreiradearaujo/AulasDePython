2217e24b89137daedf43f9d4c773b343dc82d5d4
lucas <lucasemmanuel2005@gmail.com>
2020-09-22 12:08:35 -0300

    aula90 feita

--- AulaDict/aula90.py
+++ AulaDict/aula90.py
@@ -1,3 +1,13 @@
 '''
 Faça um progroma que leia nome e média de um aluno, guardando também a situação[aprovado or nao em um dicionário. No final, mostre o conteúdo da estrutura na tela.
 '''
+
+turma = dict()
+turma['nome'] = str(input('Nome: '))
+turma['media'] = float(input('Média: '))
+if turma['media'] > 6.9:
+    turma['situacao'] = 'Aprovado'
+else:
+    turma['situacao'] ='Reprovado'
+
+print(f"Estudante {turma['nome']} com a média {turma['media']} foi {turma['situacao']}.")
