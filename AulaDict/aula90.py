054c103a1ac590079e8ff3d54d542537758379b2
lucas <lucasemmanuel2005@gmail.com>
2020-09-22 10:00:35 -0300

    exe dict

---
+++ AulaDict/aula90.py
@@ -0,0 +1,3 @@
+'''
+Faça um progroma que leia nome e média de um aluno, guardando também a situação[aprovado or nao em um dicionário. No final, mostre o conteúdo da estrutura na tela.
+'''
---
+++ AulaDict/aula92.py
@@ -0,0 +1,3 @@
+'''
+Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionario se por acaso a CTPS for diferente de ZERO, o diconario recebera também o ano de contratação e o salario. Calcule e acrescente, alem da idade, com quantos anos a pessoa vai se aposentar.
+'''
