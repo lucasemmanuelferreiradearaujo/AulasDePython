c3b3f08b1503388277f57ea2d17791c960217597
lucas <lucasemmanuel2005@gmail.com>
2020-09-23 15:54:04 -0300

    resolvido

--- AulaDict/aula92.py
+++ AulaDict/aula92.py
@@ -1,3 +1,24 @@
 '''
 Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionario se por acaso a CTPS for diferente de ZERO, o diconario recebera também o ano de contratação e o salario. Calcule e acrescente, alem da idade, com quantos anos a pessoa vai se aposentar.
 '''
+import datetime
+
+dados =dict()
+hj = datetime.date.today()
+
+ 
+dados['nome'] = str(input('Nome: '))
+dados['ano'] = int(input('Ano de nascimento: '))
+dados['carteira'] = int(input('CTPS: '))
+
+if dados['carteira'] <= 0:
+    del dados['carteira']
+else:
+    dados['anoContrato'] = int(input('Ano de contratação: '))
+    dados['salario'] = float(input('Salario: '))
+    dados['aposentadoria'] = 32 - (hj.year - dados['anoContrato'])
+
+dados['ano'] = hj.year - dados['ano']
+
+for chave, valor in dados.items():
+    print(f'chave = {chave}, valor = {valor}')
--- AulaDict/aula93.py
+++ AulaDict/aula93.py
@@ -1,3 +1,18 @@
 '''
 Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jagodor e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será guardado em um dicionario, incluindo o total de gols feitos durante o campeonato.
 '''
+
+dados={}
+cont = 0
+
+dados['nome'] = str(input('Nome do jogador: '))
+numero_de_jogos = int(input('Jogou quantos jogos? '))
+print('')
+for c in range(numero_de_jogos):
+    dados[f'jogo{c}'] = aux = int(input(f'Quantos gols fez no jogo {c}: '))
+    cont += aux
+
+print(f"O jogador {dados['nome']} fez um total de {cont} gols no campionato atual.\n")
+for chave, valor in dados.items():
+    if valor != dados['nome']:
+        print(f"No {chave} fez {valor} gols.")
