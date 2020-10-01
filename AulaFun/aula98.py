ba27e4fd31c7ad3deb2732092d5ffad3af820006
lucas <lucasemmanuel2005@gmail.com>
2020-10-01 14:03:05 -0300

    melhora

--- AulaFun/aula98.py
+++ AulaFun/aula98.py
@@ -7,18 +7,24 @@ a) De 1 até 10, de 1 em 1
 b) De 10 até 0, de 2 em 2
 c) Uma contagem personalizada
 '''
-
-
 def contador(inicio,fim,passo):
-    if inicio > fim:
-        for c in range(inicio,fim-passo,passo*-1):
+    print(f'A contagem começa em {inicio} e terminar em {fim} de {passo} em {passo}.')
+    if passo > 0:
+        if inicio > fim:
+            for c in range(inicio,fim-passo,passo*-1):
+                print(c,end=' ')
+            print('fim')
+
+        else:
+            for c in range(inicio,fim+1,passo):
+                print(c,end=' ')
+            print('fim')
+
+    else:   
+        for c in range(inicio,fim-passo,passo):
             print(c,end=' ')
         print('fim')
-    else:
-        for c in range(inicio,fim+1,passo):
-            print(c,end=' ')
-        print('fim')    
 
 contador(1,10,2)
 contador(10,0,2)
-contador(12,2,2)
+contador(int(input('Inicio:')),int(input('Fim:')),int(input('passo:')))
