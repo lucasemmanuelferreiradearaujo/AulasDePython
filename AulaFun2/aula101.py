4fc8c225fa3ac28b8d4ec31e322fda83fcaaaa0c
lucas <lucasemmanuel2005@gmail.com>
2020-10-04 08:42:18 -0300

    feito

--- AulaFun2/aula101.py
+++ AulaFun2/aula101.py
@@ -3,8 +3,11 @@
 '''
 Crie um programa que tenha uma função chamada vota() que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGATIVO, POSITIVO ou OBRIGATÓRIO nas eleições.
 '''
+from datetime import datetime
+now = datetime.now()
 
 def vota(idade=0):
+    idade = now.year-idade
     if idade > 18 and idade < 65:
         return 'Voto OBRIGATORIO'
     if idade < 18 and idade >= 16 or idade > 65:
@@ -13,4 +16,4 @@ def vota(idade=0):
         return 'Voto NEGADO'
 
 
-print(vota(int(input('Digite a idade: '))))
+print(vota(int(input('Digite o ano de nascimento: '))))
