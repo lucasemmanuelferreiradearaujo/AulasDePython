d6cad0c45cafd8af21c9fbed8c1ae7b6746466ff
lucas <lucasemmanuel2005@gmail.com>
2020-10-06 13:49:09 -0300

    resolvi

--- AulaFun2/aula102.py
+++ AulaFun2/aula102.py
@@ -3,3 +3,19 @@
 '''
 Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique o n° a calcular e o outro chamado show, que será um valor lógico(opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
 '''
+
+def fatorial(num=1, show=False):
+    if show:
+        n_fat = 1
+        print('Começando...', end=' ')
+        for i in range(2,num+1):
+            n_fat = n_fat * i
+            print(n_fat,end=' ')
+        print('!')
+    else:
+        n_fat = 1
+        for i in range(2,num+1):
+            n_fat = n_fat * i
+
+        print(f'{n_fat}!')
+fatorial(int(input('Valor: ')),show=True)
--- AulaFun2/aula103.py
+++ AulaFun2/aula103.py
@@ -5,3 +5,13 @@ Faça um programa que tenha uma função chamada ficha(), que receba dois parâm
 
 O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
 '''
+
+def ficha(nome,gols):
+    if nome in '':
+        nome = 'desconhecido'
+    if gols == '':
+        gols = '0'
+    print(f'O jogador {nome} fez {gols} gols no campionato.')
+
+
+ficha(str(input('Nome do jogador: ')).strip(' '),str(input('Gols: ')).strip(' '))
