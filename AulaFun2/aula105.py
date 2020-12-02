28fd84442a3c6d6fc756298dd145b47386a542c9
Lucas Emmanuel <63796828+lucasemmanuelferreiradearaujo@users.noreply.github.com>
2020-12-02 10:45:10 -0300

    Update aula105.py

--- AulaFun2/aula105.py
+++ AulaFun2/aula105.py
@@ -11,3 +11,21 @@ Faça um programa que tenha uma função notas() que pode receber várias notas 
 
 Adicione também as docstrings da função.
 '''
+import numpy
+
+def nota(num):
+    totalDeNotas = len(num)
+    menorNota = min(num)
+    maiorNota = max(num)
+    mediaNota = numpy.mean(num)
+    lista = [totalDeNotas, menorNota, maiorNota, mediaNota]
+    return lista
+
+notas = []
+while True:
+    notas.append(float(input('Digite a nota do aluno: ')))
+    if str(input('Continuar: ')) in 'Nn':
+        break
+
+dados = nota(notas)
+print(dados)
