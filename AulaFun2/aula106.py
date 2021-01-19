adb54a688abe4110ea60af6cd98fc4104c22f8de
Lucas Emmanuel <63796828+lucasemmanuelferreiradearaujo@users.noreply.github.com>
2021-01-19 10:08:04 -0300

    Update aula106.py
    resposta aula 106

--- AulaFun2/aula106.py
+++ AulaFun2/aula106.py
@@ -5,3 +5,35 @@ Faça um mini-sistema que utilize o intractive Help do Python. O usuário vai di
 
 OBS: use cores.
 '''
+c = (
+    '\033[m',       #0 - sem cor            
+    '\033[0;30;41m',#1 - red
+    '\033[0;30;42m',#2 - green
+    '\033[0;30;43m',#3 - yellow
+    '\033[0;30;44m',#4 - blue
+    '\033[0;30;45m',#5 - roxo
+    '\033[7;30m',#6 - branco
+)
+
+def ajuda(com):
+    titulo(f'Acessando o manual do comando \'{com}\'', 4)
+    print(c[6], end="")
+    help(com)
+    print(c[0], end='')
+
+
+def titulo(msg, cor=0):
+    tam = len(msg) + 4
+    print(c[cor], end='')
+    print('~' * tam)
+    print(f'{msg}')
+    print('~' * tam)
+    print(c[0], end='')
+
+while True:
+    titulo('Sitema de ajuda pyhelp', 2)
+    comando = str(input("função ou biblioteca > "))
+    if comando.upper() == "FIM":
+        break
+    else:
+        ajuda(comando)
