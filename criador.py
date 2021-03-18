e40aebacd52b79e48db4343a6e97ae65158a7e11
Lucas Emmanuel <63796828+lucasemmanuelferreiradearaujo@users.noreply.github.com>
2021-03-18 13:40:17 -0300

    update

--- criador.py
+++ criador.py
@@ -1,9 +1,9 @@
 from os import write
 
 
-for c in range(1,7):
-    arquivo = open(f'aula10{c}.py','w')
-    arquivo.write(f"#Aula 10{c}\n")
+for c in range(107,115):
+    arquivo = open(f'aula{c}.py','w')
+    arquivo.write(f"#Aula {c}\n")
     arquivo.write(f"#Desafio:\n")
     arquivo.write("'''\n\n")
     arquivo.write("'''")
---
+++ teste.py
@@ -0,0 +1,10 @@
+mes = []
+
+
+def casdrato_de_dados(self):
+    pass
+    self.mes.append()
+
+def mostra_dados(self):
+    pass
+
